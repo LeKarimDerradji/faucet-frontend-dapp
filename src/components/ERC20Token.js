69911c16c39cb8cb6d283bf855e34f9c5bad4ee6
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-06-18 01:40:06 +0200

    color+accordion

--- src/components/ERC20Token.js
+++ src/components/ERC20Token.js
@@ -11,87 +11,227 @@ import {
   FormErrorMessage,
   FormHelperText,
   Button,
+  Box,
   ButtonGroup,
 } from "@chakra-ui/react";
 
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react"
+
 const ERC20Token = () => {
   return (
-    <>
-      <Container maxW="container.lg">
+    <> 
+    
+      <Container maxW="100vh">
+      <HStack display='flex' justifyContent='space-around' mb={3}>
+            <StackDivider />
+            <Button w={100} colorScheme="purple">decimals</Button>
+            <Button colorScheme="purple">name</Button>
+            <Button colorScheme="purple">symbol</Button>
+            <Button colorScheme="purple">totalSupply</Button>
+          </HStack>
+    
         <VStack
           spacing={4}
           align="stretch"
-          divider={<StackDivider borderColor="gray.200" />}
         >
-          <FormControl id="fn1" spacing={3}>
-            <HStack mb={2}>
-              <Input variant="filled" placeholder="spender" size="md" />
-              <FormLabel>
-                <Button colorScheme="orange">Approve</Button>
-              </FormLabel>
-              <Input variant="filled" placeholder="amount" size="md" />
-            </HStack>
-          </FormControl>
-          <FormControl id="fn2" spacing={3}>
-            <HStack mb={2}>
-              <Input variant="filled" placeholder="spender" size="md" />
-              <FormLabel>
-                <Button colorScheme="orange">decreaseAllowance</Button>
-              </FormLabel>
-              <Input
-                variant="filled"
-                placeholder="substracted value"
-                size="md"
-              />
-            </HStack>
-          </FormControl>
-          <FormControl id="fn3" spacing={3}>
-            <HStack mb={2}>
-              <Input variant="filled" placeholder="spender" size="md" />
-              <FormLabel>
-                <Button colorScheme="orange">increaseAllowance</Button>
-              </FormLabel>
-              <Input variant="filled" placeholder="added value" size="md" />
-            </HStack>
-          </FormControl>
-          <FormControl id="fn4" spacing={3}>
-            <HStack mb={2}>
-              <Input variant="filled" placeholder="recipient" size="md" />
-              <FormLabel>
-                <Button colorScheme="orange">Transfer</Button>
-              </FormLabel>
-              <Input variant="filled" placeholder="amount" size="md" />
-            </HStack>
-          </FormControl>
-          <FormControl id="fn5" spacing={3}>
-            <HStack mb={2}>
-              <Input variant="filled" placeholder="sender" size="md" />
-              <Input variant="filled" placeholder="recipient" size="md" />
-              <Input variant="filled" placeholder="amount" size="md" />
-            </HStack>
-            <Center>
-              <FormLabel>
-                <Button colorScheme="orange">TransferFrom</Button>
-              </FormLabel>
-            </Center>
-          </FormControl>
-          <StackDivider borderColor="gray.200" />
-          <HStack spacing={8}>
-            <Button colorScheme="blue">allowance</Button>
-            <Input variant="filled" placeholder="owner" size="md" />
-            <Input variant="filled" placeholder="spender" size="md" />
-          </HStack>
-          <HStack spacing={8}>
-            <Button colorScheme="blue">balanceOf</Button>
-            <Input variant="filled" placeholder="account" size="md" />
-          </HStack>
-          <HStack spacing={8}>
-            <StackDivider borderColor="gray.200" />
-            <Button colorScheme="blue">decimals</Button>
-            <Button colorScheme="blue">name</Button>
-            <Button colorScheme="blue">symbol</Button>
-            <Button colorScheme="blue">totalSupply</Button>
-          </HStack>
+
+          <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+            <AccordionItem>
+
+              <AccordionButton backgroundColor='purple.900'>
+                <Box colorScheme='purple' flex="1" textAlign="left">
+                  Approve
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+
+              <AccordionPanel pb={4}>
+                <FormControl id="fn1" spacing={1}>
+                  <HStack>
+                    <Input placeholder="spender" />
+                    <Button colorScheme="purple">Approve</Button>
+                    <Input placeholder="amount" />
+                  </HStack>
+                </FormControl>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+            <AccordionItem>
+
+              <AccordionButton backgroundColor='purple.900'>
+                <Box colorScheme='purple' flex="1" textAlign="left">
+                  Decrease Allowance
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+
+              <AccordionPanel pb={4}>
+
+                <FormControl id="fn2" spacing={3}>
+                  <HStack mb={2}>
+                    <Input placeholder="spender" size="md" />
+                    <FormLabel>
+                      <Button colorScheme="purple">decreaseAllowance</Button>
+                    </FormLabel>
+                    <Input
+                      placeholder="substracted value"
+                      size="md"
+                    />
+                  </HStack>
+                </FormControl>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+            <AccordionItem>
+
+              <AccordionButton backgroundColor='purple.900'>
+                <Box colorScheme='purple' flex="1" textAlign="left">
+                  Increase Allowance
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+
+              <AccordionPanel pb={4}>
+
+                <FormControl id="fn3" spacing={3}>
+                  <HStack mb={2}>
+                    <Input colorScheme='purple' placeholder="spender" size="md" />
+                    <FormLabel>
+                      <Button colorScheme="purple">increaseAllowance</Button>
+                    </FormLabel>
+                    <Input placeholder="added value" size="md" />
+                  </HStack>
+                </FormControl>
+
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+
+          <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+            <AccordionItem>
+
+              <AccordionButton backgroundColor='purple.900'>
+                <Box colorScheme='purple' flex="1" textAlign="left">
+                  Transfer
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+
+              <AccordionPanel pb={4}>
+
+                <FormControl id="fn4" spacing={3}>
+                  <HStack mb={2}>
+                    <Input placeholder="recipient" size="md" />
+                    <FormLabel>
+                      <Button colorScheme="purple">Transfer</Button>
+                    </FormLabel>
+                    <Input placeholder="amount" size="md" />
+                  </HStack>
+                </FormControl>
+
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+
+
+
+          <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+            <AccordionItem>
+
+              <AccordionButton backgroundColor='purple.900'>
+                <Box colorScheme='purple' flex="1" textAlign="left">
+                  Transfer From
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+
+              <AccordionPanel pb={4}>
+
+                <FormControl id="fn5" spacing={3}>
+                  <HStack mb={2}>
+                    <Input placeholder="sender" size="md" />
+                    <Input placeholder="recipient" size="md" />
+                    <Input placeholder="amount" size="md" />
+                  </HStack>
+                  <Center>
+                    <FormLabel>
+                      <Button colorScheme="purple">TransferFrom</Button>
+                    </FormLabel>
+                  </Center>
+                </FormControl>
+
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+
+
+
+          <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+            <AccordionItem>
+
+              <AccordionButton backgroundColor='purple.900'>
+                <Box colorScheme='purple' flex="1" textAlign="left">
+                  Transfer From
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+
+              <AccordionPanel pb={4}>
+
+                <FormControl id="fn5" spacing={3}>
+                  <HStack mb={2}>
+                    <Input placeholder="sender" size="md" />
+                    <Input placeholder="recipient" size="md" />
+                    <Input placeholder="amount" size="md" />
+                  </HStack>
+                  <Center>
+                    <FormLabel>
+                      <Button colorScheme="purple">TransferFrom</Button>
+                    </FormLabel>
+                  </Center>
+                </FormControl>
+
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+
+
+          <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+            <AccordionItem>
+
+              <AccordionButton backgroundColor='purple.900'>
+                <Box colorScheme='purple' flex="1" textAlign="left">
+                  Spy on others
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+
+              <AccordionPanel pb={4}>
+
+                <StackDivider />
+                <HStack>
+                  <Button colorScheme="purple">allowance</Button>
+                  <Input placeholder="owner" size="md" />
+                  <Input placeholder="spender" size="md" />
+                </HStack>
+                <HStack >
+                  <Button colorScheme="purple">balanceOf</Button>
+                  <Input placeholder="account" size="md" />
+                </HStack>
+
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
         </VStack>
       </Container>
     </>
